96995188f6155d959fd955f2a3d511bb46548cb3
Allison MacQueen <allimacq@gmail.com>
2020-11-11 17:34:39 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,26 @@
+class Appointment
+  
+  attr_accessor :date, :patients, :doctor
+  
+  @@all = [ ]
+  
+  def initialize(date, patients, doctor)
+    @date = date
+    @patients = patients
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def patient
+    self.patients
+  end
+  
+  #def doctor
+   # p self
+  #end
+  
+end
---
+++ lib/patient.rb
@@ -0,0 +1,35 @@
+class Patient
+  
+  attr_accessor :name, :date, :doctor, :appointments
+  
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(date, doctor)
+    new_apt = Appointment.new(date, self, doctor)
+    p new_apt
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    apt = self
+    #apt.new_appointment(@date,@doctor)
+  end
+  
+  def doctors
+    doctors_list = [ ]
+    Appointment.all.each do |appointment|
+      doctors_list << appointment.doctor
+    end
+    doctors_list
+  end
+  
+  
+end
